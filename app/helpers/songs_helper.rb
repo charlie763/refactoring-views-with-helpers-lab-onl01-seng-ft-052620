6e367aae2df1cb3b6bcddbfe4d0baffc9dcd31e4
charlie763 <cwisoff@gmail.com>
2020-08-05 09:10:16 -0600

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,6 @@
 module SongsHelper
-
+  def display_artist(song)
+    artist = song.artist
+    artist ? link_to(artist.name, artist_path(artist)) : link_to("Add Artist", edit_song_path(song))
+  end
 end
